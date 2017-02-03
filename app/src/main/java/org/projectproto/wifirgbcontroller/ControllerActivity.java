0296a091fd319766dc2c4da03527794066df3e39
yus <yus.projectproto@gmail.com>
2017-02-03 20:51:11 +0800

    support for newer android versions

--- app/src/main/java/org/projectproto/wifirgbcontroller/ControllerActivity.java
+++ app/src/main/java/org/projectproto/wifirgbcontroller/ControllerActivity.java
@@ -1,6 +1,9 @@
 package org.projectproto.wifirgbcontroller;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,7 +22,16 @@ public class ControllerActivity extends AppCompatActivity {
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startService(new Intent(ControllerActivity.this, FloatingWindow.class));
+                if (Build.VERSION.SDK_INT >= 23) {
+                    if (!Settings.canDrawOverlays(ControllerActivity.this)) {
+                        Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                                Uri.parse("package:" + getPackageName()));
+                        startActivityForResult(intent, 1234);
+                        return;
+                    }
+                }
+                Intent intent = new Intent(ControllerActivity.this, FloatingWindow.class);
+                startService(intent);
             }
         });
     }
