4e50f96998c41ad0df2600e2a7a05d23ca871105
yus <yus.projectproto@gmail.com>
2017-02-03 22:54:32 +0800

    initial widgets

--- app/src/main/java/org/projectproto/wifirgbcontroller/FloatingWindow.java
+++ app/src/main/java/org/projectproto/wifirgbcontroller/FloatingWindow.java
@@ -10,9 +10,9 @@ import android.view.Display;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 
 public class FloatingWindow extends Service{
@@ -20,6 +20,8 @@ public class FloatingWindow extends Service{
     private WindowManager wm;
     private RelativeLayout layout;
     private Button btnStop;
+    private EditText txtInput;
+    private Button btnSetText;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -33,18 +35,45 @@ public class FloatingWindow extends Service{
         wm = (WindowManager) getSystemService(WINDOW_SERVICE);
         layout = new RelativeLayout(this);
         btnStop = new Button(this);
+        txtInput = new EditText(this);
+        btnSetText = new Button(this);
 
-        ViewGroup.LayoutParams blParams = new ViewGroup.LayoutParams(
-                                                    ViewGroup.LayoutParams.WRAP_CONTENT,
-                                                    ViewGroup.LayoutParams.WRAP_CONTENT );
+        RelativeLayout.LayoutParams btnStopLayourParams =
+                new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT);
+
         btnStop.setText("Exit");
-        btnStop.setLayoutParams(blParams);
+        btnStopLayourParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
+        btnStopLayourParams.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
+        btnStop.setLayoutParams(btnStopLayourParams);
 
-        RelativeLayout.LayoutParams layoutParamsParams = new RelativeLayout.LayoutParams(
+        RelativeLayout.LayoutParams txtInputLayourParams =
+                new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.MATCH_PARENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        txtInput.setHint("message");
+        txtInputLayourParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
+        txtInputLayourParams.addRule(RelativeLayout.BELOW, btnStop.getId());
+        txtInput.setLayoutParams(txtInputLayourParams);
+
+        RelativeLayout.LayoutParams btnSetTextLayourParams =
+                new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        btnSetText.setText("Set");
+        btnSetTextLayourParams.addRule(RelativeLayout.CENTER_IN_PARENT);
+        //btnSetTextLayourParams.addRule(RelativeLayout.BELOW, txtInput.getId());
+        btnSetText.setLayoutParams(btnSetTextLayourParams);
+
+
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
                                                         RelativeLayout.LayoutParams.MATCH_PARENT,
                                                         RelativeLayout.LayoutParams.MATCH_PARENT);
         layout.setBackgroundColor(Color.argb(96, 152, 255, 152));
-        layout.setLayoutParams(layoutParamsParams);
+        layout.setLayoutParams(layoutParams);
 
         Display display = wm.getDefaultDisplay();
         Point size = new Point();
@@ -53,14 +82,15 @@ public class FloatingWindow extends Service{
                                                     (size.x * 7) / 10 /* width */,
                                                     (size.y * 3) / 10 /* height */,
                                                     WindowManager.LayoutParams.TYPE_PHONE,
-                                                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                                                    WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
                                                     PixelFormat.TRANSLUCENT );
 
-        wlparams.x = 0;
-        wlparams.y = 0;
+
         wlparams.gravity = Gravity.CENTER;
 
         layout.addView(btnStop);
+        layout.addView(txtInput);
+        layout.addView(btnSetText);
         wm.addView(layout, wlparams);
 
         layout.setOnTouchListener(new View.OnTouchListener() {
@@ -75,13 +105,15 @@ public class FloatingWindow extends Service{
                     y = nextparams.y;
                     touchedX = event.getRawX();
                     touchedY = event.getRawY();
-                    break;
+                    return true;
+                case MotionEvent.ACTION_UP:
+                    return true;
                 case MotionEvent.ACTION_MOVE:
                     nextparams.x = (int) (x + event.getRawX() - touchedX);
                     nextparams.y = (int) (y + event.getRawY() - touchedY);
 
                     wm.updateViewLayout(layout, nextparams);
-                    break;
+                    return true;
                 }
                 return false;
             }
